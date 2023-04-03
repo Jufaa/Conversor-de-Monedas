4ca251132273fc4f3f8b2f37cf2347fda4a4bd9c
Juan Cruz Francitorra <jufovich@gmail.com>
2023-04-03 10:56:36 -0300

    Faltan validaciones!

--- src/GUI/ConvertirPesosAMonedas.java
+++ src/GUI/ConvertirPesosAMonedas.java
@@ -7,26 +7,26 @@ public class ConvertirPesosAMonedas {
   public void PesoADolar(double monto){
     double PaD = monto * 0.0048;
     DecimalFormat df = new DecimalFormat("#.##");
-    PaD = Double.valueOf(df.format(PaD));
+    PaD = Double.parseDouble(df.format(PaD));
     JOptionPane.showMessageDialog(null, "Total: $" + PaD + "!");
   }
 
   public void PesoAEuros(double monto){
     double PaEu = monto * 0.0045;
     DecimalFormat df2 = new DecimalFormat("#.##");
-    PaEu = Double.valueOf(df2.format(PaEu));
+    PaEu = Double.parseDouble(df2.format(PaEu));
     JOptionPane.showMessageDialog(null, "Total: $" + PaEu + "!");
   }
   public void PesoALibras(double monto){
     double PaES = monto * 0.0039;
     DecimalFormat df3 = new DecimalFormat("#.##");
-    PaES = Double.valueOf(df3.format(PaES));
+    PaES = Double.parseDouble(df3.format(PaES));
     JOptionPane.showMessageDialog(null, "Total: $" + PaES + "!");
   }
   public void PesoAYen(double monto){
     double PaY = monto * 0.64;
     DecimalFormat df4 = new DecimalFormat("#.##");
-    PaY = Double.valueOf(df4.format(PaY));
+    PaY = Double.parseDouble(df4.format(PaY));
     JOptionPane.showMessageDialog(null, "Total: $" + PaY + "!");
 
   }
@@ -34,7 +34,7 @@ public class ConvertirPesosAMonedas {
   public void PesoAWon(double monto){
     double PaW = monto * 6.26;
     DecimalFormat df5 = new DecimalFormat("#.##");
-    PaW = Double.valueOf(df5.format(PaW));
+    PaW = Double.parseDouble(df5.format(PaW));
     JOptionPane.showMessageDialog(null, "Total: $" + PaW + "!");
   }
 }
